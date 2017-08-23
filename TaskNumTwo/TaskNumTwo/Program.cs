6d7f77ac7c1356022f96192918a6e09b1c55e5a0
TemaSolovei <temasolovei@gmail.com>
2017-08-24 03:25:04 +0500

    Пропись алгоритма

---
+++ TaskNumTwo/TaskNumTwo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskNumTwo
+{
+    class Program
+    {
+        private static int size;
+        private static int[,] masPoints;
+
+        static void Main(string[] args)
+        {
+
+        }
+
+        static void IORead()
+        {
+            size = Int32.Parse(Console.ReadLine());
+            
+            for (int i = 0; )
+        }
+    }
+}

c508494319b61e825a6535945c48b1f978293a6f
TemaSolovei <temasolovei@gmail.com>
2017-08-24 15:18:33 +0500

    Прописал сортировку в введённом массиве

--- TaskNumTwo/TaskNumTwo/Program.cs
+++ TaskNumTwo/TaskNumTwo/Program.cs
@@ -13,14 +13,37 @@ namespace TaskNumTwo
 
         static void Main(string[] args)
         {
-
+            IORead();
         }
 
         static void IORead()
         {
             size = Int32.Parse(Console.ReadLine());
+            masPoints = new int[size,4];
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    masPoints[i,j] = Int32.Parse(Console.ReadLine());
+                }
+
+                if ((masPoints[i, 0] + masPoints[i, 1]) < (masPoints[i, 2] + masPoints[i, 3]))
+                {
+                    int tempX = masPoints[i, 0], tempY = masPoints[i, 1];
+                    masPoints[i, 0] = masPoints[i, 2];
+                    masPoints[i, 1] = masPoints[i, 3];
+                    masPoints[i, 2] = tempX;
+                    masPoints[i, 3] = tempY;
+                }
+            }
+        }
+
+        static void Calculate()
+        {
+            int x1, y1, x2, y2, x1Old, y1Old, x2Old, y2Old;
             
-            for (int i = 0; )
+
         }
     }
 }
